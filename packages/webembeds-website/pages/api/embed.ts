7c83395db4be25b75a746b7ef3f4a279ca2b4aad
Girish Patil <devegpat@gmail.com>
2021-02-10 12:00:01 +0530

    feat: set cache header

--- packages/webembeds-website/pages/api/embed.ts
+++ packages/webembeds-website/pages/api/embed.ts
@@ -21,6 +21,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
 
+  res.setHeader("Cache-Control", "s-maxage=2592000");
+
   // Twitch needs a parent url where the embed is being used.
   const host = req.headers.host;
 
