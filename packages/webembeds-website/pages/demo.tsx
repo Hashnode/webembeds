bfcb99e035a38e9e9ad7a5c91ccd1184d011c348
Girish Patil <devegpat@gmail.com>
2021-02-22 02:24:33 +0530

    docs: update demo

--- packages/webembeds-website/pages/demo.tsx
+++ packages/webembeds-website/pages/demo.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
-import React, { useState, useEffect, useRef, ReactNode } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Loader from "../components/Loader";
 
 const links: any = {
 	spotify: "https://open.spotify.com/track/3G8o2zm7LaF6eeVuvLlrkJ?si=Sx1sCnhDT6GXqSLIwSLOeQ",
@@ -8,7 +9,6 @@ const links: any = {
 	codepen: "https://codepen.io/bsehovac/pen/EMyWVv",
 	youtube: "https://www.youtube.com/watch?v=32I0Qso4sDg",
 	twitter: "https://twitter.com/hashnode/status/1352525138659430400",
-	instagram: "https://www.instagram.com/p/CJ2ja7Tl3S5/",
 	glitch: "https://glitch.com/edit/#!/remote-hands",
 	expo: "https://snack.expo.io/@girishhashnode/unnamed-snack",
 	twitch: "https://www.twitch.tv/fresh",
@@ -20,27 +20,34 @@ const links: any = {
 	anchor: "https://anchor.fm/startapodcast/episodes/Whats-your-podcast-about-e17krq/a-a2q3ft",
 	loom: "https://www.loom.com/share/0281766fa2d04bb788eaf19e65135184",
 	vimeo: "https://vimeo.com/336812660",
-	// facebook: "https://www.facebook.com/MoHFWIndia/posts/1757090964450303",
 	fallback: "https://hashnode.com",
 };
 
 function Demo() {
 	const urlRef = useRef<HTMLInputElement>(null);
-	const [result, setResult] = useState<{ output: { html?: string }, error: boolean }>();
+	const embedRef = useRef<HTMLInputElement>(null);
+	const [result, setResult] = useState<{ output: { html?: string }; error: boolean }>();
+	const [isLoading, setLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		handleURL(links.anchor);
+	}, []);
 
 	const handleURL = async (incomingURL?: string) => {
-    if (null === urlRef) {
-      return;
-    }
-    if (null === urlRef.current) {
-      return;
-    }
+		if (null === urlRef) {
+			return;
+		}
+		if (null === urlRef.current) {
+			return;
+		}
 
 		const url = incomingURL || (urlRef !== null ? urlRef.current.value : null);
 
 		if (!url) {
 			return;
 		}
+
+		setLoading(true);
 
 		// https://webembeds.com
 		const requestURL = `/api/embed?url=${encodeURIComponent(url)}`;
@@ -51,51 +58,76 @@ function Demo() {
 				"Content-Type": "application/json",
 			},
 		});
-    const json = await response.json();
+		const json = await response.json();
+
+		setLoading(false);
 		setResult(json ? json.data : null);
 	};
 
 	return (
 		<div className="w-full mx-auto max-w-4xl">
-			<h1 className="text-4xl font-semibold text-center py-12">Webembeds</h1>
-			<div className="flex items-center">
-        <input className="border border-black w-full p-4 text-lg" placeholder={"Enter url"} type="url" ref={urlRef} />
-        <button className="p-4 bg-green-300 w-1/6 border-2 border-transparent text-lg" type="button" onClick={() => { handleURL(); }}>
-          Submit
-        </button>
-      </div>
+			<div className="max-w-3xl mx-auto">
+				<h1 className="text-xl font-semibold py-6 px-3">Webembeds</h1>
+			</div>
 
-      <p className="py-4 text-center">Or select from below</p>
-
-			<div>
-				{Object.keys(links).map((key, index) => {
-					return (
-						<button
-							className="m-2 p-2 bg-gray-700 text-white"
-							key={index}
-							onClick={() => handleURL(links[key])}
-						>
-							{key}
-						</button>
-					);
-				})}
+			<div className="flex flex-col">
+				{/* <div>
+          {result ? <div className="h-72 overflow-y-scroll leading-relaxed">{JSON.stringify(result)}</div> : "No result"}
+        </div> */}
+				{isLoading ? <Loader /> : null}
+				{result && !result.error && !isLoading ? (
+					<div
+						className="shadow-2xl p-2 bg-white rounded-sm border border-gray-200"
+						dangerouslySetInnerHTML={{ __html: result.output.html || "" }}
+						// ref={embedRef}
+					/>
+				) : null}
+				{result && result.error ? "Something went wrong" : ""}
 			</div>
 
 			<br />
 
-      <div className="grid grid-cols-3">
-        <div>
-          {result ? <div className="h-72 overflow-y-scroll leading-relaxed">{JSON.stringify(result)}</div> : "No result"}
-        </div>
-        <div className="col-span-2">
-          {result && !result.error ? (
-            <div className="px-2" dangerouslySetInnerHTML={{ __html: result.output.html || "" }} />
-          ) : null}
-          {
-            result && result.error ? "Something went wrong" : ""
-          }
-        </div>
-      </div>
+			<div className="max-w-3xl mx-auto">
+				<div className="flex items-center">
+					<input
+						className="border border-gray-500 rounded-sm w-full p-3 px-6 text-lg mr-2 cursor-pointer"
+						placeholder={"Enter url"}
+						type="url"
+						ref={urlRef}
+					/>
+					<button
+						className="p-3 bg-blue-700 text-white w-1/6 border-2 border-transparent text-lg rounded-sm"
+						type="button"
+						onClick={() => {
+							handleURL();
+						}}
+					>
+						Submit
+					</button>
+				</div>
+
+				<p className="py-4 text-center">Or select from below</p>
+
+				<div>
+					{Object.keys(links).map((key, index) => {
+						return (
+							<button
+								className="mx-1 my-1 p-1 px-4 bg-gray-700 text-white rounded-sm capitalize"
+								key={index}
+								onClick={() => handleURL(links[key])}
+							>
+								{key}
+							</button>
+						);
+					})}
+				</div>
+			</div>
+			<div className="max-w-3xl mx-auto pt-32 pb-4 text-center">
+				Made with ❤️ &nbsp; by{" "}
+				<a href="https://hashnode.com" className="text-blue-700">
+					Hashnode
+				</a>
+			</div>
 		</div>
 	);
 }
