6d5e597dc121255dc693749675272a8e4c5c058a
Girish Patil <devegpat@gmail.com>
2021-02-22 00:08:05 +0530

    test: basic core test setup

---
+++ packages/webembeds-core/jest.config.ts
@@ -0,0 +1,20 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  testEnvironment: 'node',
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node",
+  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+};
